dea869094461497c6ba4d7c337b7c25463d23d9b
Renata Albuquerque <renataalbq1@gmail.com>
2023-05-01 09:28:21 -0300

    logout function

--- src/pages/settings/settings.tsx
+++ src/pages/settings/settings.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { Container, VSeparator, VView } from '../../components/layout/layout';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -6,12 +6,14 @@ import { Switch } from 'react-native';
 import { ButtonLogout, Content, Setting, SettingsOption, TextLogoutButton, Title } from './settings.style';
 import { Header } from '../../components/header/header';
 import { Divider } from '../../components/header/header.style';
+import { UserContext } from '../../store/user-context';
 
 export const SettingsPage = () => {
   const navigation = useNavigation();
   const [isAlphaOrder, setIsAlphaOrder] = useState(false);
   const [isReleaseDateOrder, setIsReleaseDateOrder] = useState(false);
-  const [order, setOrder]  = useState('');
+  const [order, setOrder] = useState('');
+  const {logout, logged} = useContext(UserContext)
 
   const handleToggle = async (value: string) => {
     setOrder(value);
@@ -24,7 +26,10 @@ export const SettingsPage = () => {
   };
 
   function handleLogout() {
-    navigation.navigate('Login')
+    logout();
+    if (!logged) {
+      navigation.navigate('Login')
+    }
   }
 
   return (
